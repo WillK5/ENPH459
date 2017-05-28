492d92f2da0131462b9b010e132183ca1545eabd
William Kemp <William Kemp>
2017-05-27 18:56:27 -0700

    Updated serial read

--- RFIDSerialRead.py
+++ RFIDSerialRead.py
@@ -1,10 +1,42 @@
 import serial
 
-serial = serial.Serial("/dev/ttyUSB0" buadrate=9600)
+serial = serial.Serial("/dev/ttyUSB0", baudrate=9600)
+secureList = []
 
 code = ''
 
-while TRUE:
+while True:
 
-    data = serial.read()
-    if data == '
+    if serial.read() == b'\x02':
+        data = serial.read()
+        char = data.decode("utf-8")
+
+        while data != b'\r':
+            code += char
+            data = serial.read()
+            char = data.decode("utf-8")
+
+        print(code)
+        secureList += [code]
+        code = ''
+        print(secureList)
+
+#    data = serial.read()
+ #   print(data)
+  #  char = data.decode("utf-8")
+#
+    #print(char)
+
+ #   if char == '\r':
+  #      print('String ending')
+   #     print(code)
+
+        # Storing code for later use
+    #    secureList += [code]  
+     #   code = ''
+      #  print(secureList)
+        
+#    elif char == '\x02':
+ #       print('String starting')
+  #  else:
+   #     code = code + char
