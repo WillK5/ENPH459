f20815163bc1d27e135cb85ab18fe510eb109e64
William Kemp <kempwilliam94@alumni.ubc.ca>
2017-05-31 22:34:39 -0700

    Serial read changes

--- RFIDSerialRead.py
+++ RFIDSerialRead.py
@@ -1,38 +1,154 @@
 import serial
+import time
+import Tkinter as tk
 
 serial = serial.Serial("/dev/ttyUSB0", baudrate=9600)
+global secureList
+
 secureList = []
 
-code = ''
+##################### MAIN CODE ###############################
 
-while True:
+# Starting Scene (waiting for user to press button)
+    # Button pressed, calling RFIDAuthenticate
+    # If RFIDAuthenticate returns True -> Loading procedure begins
+    #                     returns False -> Call error message -> send to starting scene
+    # LoadingProcedure:
 
-    if serial.read() == b'\x02':
-        code = ''
-        data = serial.read()
-        char = data.decode("utf-8")
+if RFIDAuthenticate():
+    #Calling loading procedure
+    if operate(doorState):
+    	
+else: 
+    #calling starting scene 
 
-        while data != b'\r':
-            code += char
+def RFIDAuthenticate():
+    code = ''
+    timeout = 10000 #Time it takes for RFID read to timeout
+    t0 = time.time()
+    while True:
+
+        # Checking for timeout
+        if (time.time()-t0 >= timout):
+            # Return to main screen
+            return False
+
+        # Getting RFID code
+        if serial.read() == b'\x02':
+            code = ''
             data = serial.read()
             char = data.decode("utf-8")
-            
-        print(code)
-        print(secureList)
 
-    if code in secureList:        # open the door immediately, stand back image written
-        print("opening")
-        #doorOpen()
+            while data != b'\r':
+                code += char
+                data = serial.read()
+                char = data.decode("utf-8")
+                
+            print(code)
+            print(secureList)
 
-    else: 
-        # Send to payment image, wait for user to imput the image
-        print("not registered")
-        print("Payment")
-        secureList += [code]
-        # if paymentFunc():
-        #     doorOpen()
-        # else:
-        #     #Write error message, return to main screen
+        # Checking for authentication
+        if code in secureList:        # open the door immediately, stand back image written
+            print("opening")
+            return True
+            #doorOpen()
+
+        else: 
+            # Send to payment image, wait for user to imput the image
+            print("not registered")
+            print("Payment")
+            if paymentFunc():
+                secureList += [code]
+                return True
+
+            else:
+                return False
+            #     #Write error message, return to main screen
 
 
-    
+def paymentFunc():
+    # Tkinter scene prompting user to indicate they made payment
+
+    win = tk.Tk()
+    win.title("Payment scene")
+
+
+    while True:    
+        payButton = tk.Button(win, text='Payment', font=myFont, command= (result=True) # To be completed
+        notPayButton = tk.Button(win, text='No Payment', font=myFont, command= (result=False))
+
+    return result
+
+
+
+# Assuming motor has been enabled and initialized beforehand
+
+def operate(doorState):
+
+    timer = time.time()
+
+    openLast = 1
+    closedLast = 0
+
+    closingDir = 1
+    openingDir = 0
+
+    endPulses = 3000 # Time until the deceleration phase begins
+
+    speedHi = 0.00001
+    speedLo = 0.001
+
+    rampUpTime = 20 # Duration of ramp function in seconds
+
+    timeVar = speedLo # Time variable initialization (starting at zero)
+
+    accelStep = 0.0000003 # Amount of time to decrement between acceleration pulses for each iteration
+    decelTime = 0.0000003 # Amount of time to decrement between deceleration pulses for each iteration
+
+    # Checking last known state
+    if (doorState == openLast):    #Door was last open, closing
+        GPIO.output(directionPin, closingDir)
+        limitSW = GPIO.input(limitL)
+
+    if (doorState == closedLast):    #Door was last closed, opening
+        GPIO.output(directionPin, openingDir)
+        limitSW = GPIO.input(limitR)
+
+    t0 = time.time() # Starting time for actuation
+
+    while (limitSW != 1): #While limit switch is not pressed
+
+        timer = time.time()
+
+        if (((timer-t0) < rampUpTime) and (timeVar > speedHi)): # Ramp up function          
+            GPIO.output(step,1)
+            time.sleep(highPulse)
+            GPIO.output(step,0)
+            time.sleep(abs(timeVar))
+
+            timeVar = timeVar - accelStep   #Decreasing gap between pulses
+
+        elif ():
+            GPIO.output(step,1)
+            time.sleep(highPulse)
+            GPIO.output(step,0)
+            time.sleep(abs(timeVar))
+
+        elif ():
+            GPIO.output(step,1)
+            time.sleep(highPulse)
+            GPIO.output(step,0)
+            time.sleep(abs(timeVar))
+
+            timeVar = timeVar + decelTime
+
+        else:
+
+            print("Error")
+            return False
+
+    # Backoff limit switch until correct point
+
+    print("Open/closing complete")
+
+    return True
--- RampCode.py
+++ RampCode.py
@@ -2,35 +2,72 @@
 
 # Assuming motor has been enabled and initialized beforehand
 
-def operate():
+def operate(doorState):
 
-    timeVar = 0 # Time variable initialization (starting at zero)
-    accelTime = 0.0000003 # Amount of time to decrement between acceleration pulses for each iteration
+    timer = time.time()
+
+    openLast = 1
+    closedLast = 0
+
+    closingDir = 1
+    openingDir = 0
+
+    accelStep = 0.0000003 # Amount of time to decrement between acceleration pulses for each iteration
     decelTime = 0.0000003 # Amount of time to decrement between deceleration pulses for each iteration
 
-    endTime = 3000 # Time until the deceleration phase begins
+    endPulses = 3000 # Time until the deceleration phase begins
 
     speedHi = 0.00001
     speedLo = 0.001
 
+    rampUpTime = 20 # Duration of ramp function in seconds
+
+    timeVar = speedLo # Time variable initialization (starting at zero)
+
     # Checking last known state
-    if (doorState == 1):    #Door was last open, closing
-        direction = 1
-        GPIO.output(directionPin, direction)
+    if (doorState == openLast):    #Door was last open, closing
+        GPIO.output(directionPin, closingDir)
         limitSW = GPIO.input(limitL)
 
-    if (doorState == 0):    #Door was last closed, opening
-        direction = 0
-        GPIO.output(directionPin, direction)
+    if (doorState == closedLast):    #Door was last closed, opening
+        GPIO.output(directionPin, openingDir)
         limitSW = GPIO.input(limitR)
 
+    t0 = time.time() # Starting time for actuation
 
     while (limitSW != 1): #While limit switch is not pressed
 
-        GPIO.output(step,1)
-        time.sleep(highPulse)
-        GPIO.output(step,0)
-        time.sleep(abs(timeVar))
+        timer = time.time()
 
-        if ( (timeVar > endTime) or !(timeVar <= speedHi) ):
-            timeVar = timeVar - accelTime   #Decreasing gap between pulses until a certain amount of time
+        if (((timer-t0) < rampUpTime) and (timeVar > speedHi)): # Ramp up function          
+            GPIO.output(step,1)
+            time.sleep(highPulse)
+            GPIO.output(step,0)
+            time.sleep(abs(timeVar))
+
+            timeVar = timeVar - accelStep   #Decreasing gap between pulses
+
+        elif ():
+            GPIO.output(step,1)
+            time.sleep(highPulse)
+            GPIO.output(step,0)
+            time.sleep(abs(timeVar))
+
+        elif ():
+            GPIO.output(step,1)
+            time.sleep(highPulse)
+            GPIO.output(step,0)
+            time.sleep(abs(timeVar))
+
+            timeVar = timeVar + decelTime
+
+        else:
+
+            print("Error")
+            return False
+
+    # Backoff limit switch until correct point
+
+    print("Open/closing complete")
+
+    return True
